f7caadec235ef47eca6c602b703ce2284b3eb272
hard265 <hardynamakhwa@gmail.com>
2025-03-13 16:29:44 -0700

    Add Home screen and update navigation

--- router.tsx
+++ router.tsx
@@ -1,18 +1,20 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import SignIn from "./screens/auth/SignIn"
 import { observer } from "mobx-react-lite"
+import Home from "./screens/Home"
 
 const RootStack = createNativeStackNavigator()
 
 export default observer(() => {
     return (
         <RootStack.Navigator>
-            {
-                <RootStack.Group>
 
-                    <RootStack.Screen name="SignIn" component={SignIn} />
-                </RootStack.Group>
-            }
+            <RootStack.Group>
+
+                <RootStack.Screen name="SignIn" component={SignIn} />
+            </RootStack.Group>
+            <RootStack.Screen name="Home" component={Home} />
+
         </RootStack.Navigator>
     )
 })
